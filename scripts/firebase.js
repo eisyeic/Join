916b09659fc052418f02f33eae626a1587da478c
Dieter Foos <d.foos@mail.de>
2025-06-29 11:47:33 +0200

    classes added

---
+++ scripts/firebase.js
@@ -0,0 +1,20 @@
+const contact = [
+    {
+        firstName: "David",
+        lastName: "Eisenbarth",
+        email: "d.eisenbarth@mail.de",
+        color: ""
+    },
+    {
+        firstName: "Miguel",
+        lastName: "Garcia",
+        email: "m.garcia@mail.de",
+        color: ""
+    },
+     {
+        firstName: "Dieter",
+        lastName: "Foos",
+        email: "d.foos@mail.de",
+        color: ""
+    }
+]
