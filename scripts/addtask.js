1a3ec4e3a7859d5f324822745146885ff7737c32
Dieter Foos <d.foos@mail.de>
2025-07-15 22:17:46 +0200

    addtask assigned optimized and category added

---
+++ scripts/addtask.js
@@ -0,0 +1,22 @@
+const createButton = document.getElementById('create-button');
+
+const assignedSelect = document.getElementById('assigned-select-box');
+const assignedIcon = document.getElementById('assigned-icon');
+const contanctDropDown = document.getElementById('contact-list-box');
+
+const categorySelect = document.getElementById('category-select');
+const categorySelection = document.getElementById('category-selection');
+
+createButton.addEventListener("mouseover", function() {
+    createButton.classList.toggle('button-toggle');    
+});
+
+assignedSelect.addEventListener("click", function() {
+    contanctDropDown.classList.toggle('d-none');
+    assignedIcon.classList.toggle('arrow-down');
+    assignedIcon.classList.toggle('arrow-up');
+});
+
+categorySelect.addEventListener("click", function() {
+    categorySelection.classList.toggle('d-none');
+})
