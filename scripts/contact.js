2f539993fea4267acaa04268426ec34f0eabb370
eisyeic <davideisenbarth@gmail.com>
2025-07-21 23:29:23 +0200

    add contact.js and responsiv overlays finish

---
+++ scripts/contact.js
@@ -0,0 +1,11 @@
+function toggleEditContact() {
+ overlayEdit= document.getElementById("contact-overlay-close-edit");
+
+ overlayEdit.classList.toggle("d-none");
+}
+
+function toggleAddContact() {
+ overlayEdit= document.getElementById("contact-overlay-close-add");
+
+ overlayEdit.classList.toggle("d-none");
+}
