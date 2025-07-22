88948d16bbfb0fd26a8ffa8adf62044f831d257c
eisyeic <davideisenbarth@gmail.com>
2025-07-22 23:02:27 +0200

    header toggle nav bar added in all html

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // utility
 let $ = id => document.getElementById(id);
+
+// Toggle Profile Navbar
+
+function toggleProfileNavbar() {
+    let profileNavbar = $("profile-navbar");
+    if (profileNavbar.classList.contains("d-none")) {
+        profileNavbar.classList.remove("d-none");
+    } else {
+        profileNavbar.classList.add("d-none");
+    }
+}
