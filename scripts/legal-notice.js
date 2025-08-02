ba071eb3d3ac4e32fa68bed0c62a93698fafd456
eisyeic <davideisenbarth@gmail.com>
2025-08-02 10:32:50 +0200

    footer-nav updated for users and guests

--- scripts/legal-notice.js
+++ scripts/legal-notice.js
@@ -4,12 +4,18 @@ import { auth } from "./firebase.js";
 onAuthStateChanged(auth, (user) => {
   const navLoginBox = document.querySelector('.nav-login-box');
   const nav = document.querySelector('.nav');
+  const navBox = document.querySelector('.nav-box');
+  const navLoginBoxMobile = document.querySelector('.nav-login-box-mobile');
   
   if (user && user.email !== 'guest@login.de') {
     navLoginBox.classList.add('d-none');
     nav.classList.remove('d-none');
+    navBox.classList.remove('d-none');
+    navLoginBoxMobile.classList.add('d-none');
   } else {
     navLoginBox.classList.remove('d-none');
     nav.classList.add('d-none');
+    navBox.classList.add('d-none');
+    navLoginBoxMobile.classList.remove('d-none');
   }
 });
