672f4deafd4975ab0f2f39b8a0e25d8d7bde8143
eisyeic <davideisenbarth@gmail.com>
2025-08-02 11:28:41 +0200

    remove contact-initials.js and integrate user initials handling in contact-firebase.js

--- scripts/contact-initials.js
+++
@@ -1,8 +0,0 @@
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { auth } from "./firebase.js";
-
-onAuthStateChanged(auth, (user) => {
-  if (window.updateUserInitials) {
-    window.updateUserInitials(user);
-  }
-});
