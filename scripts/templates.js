75ebac249e703c77b4fef2a46ebad7f1d957771e
eisyeic <davideisenbarth@gmail.com>
2025-08-22 22:23:06 +0200

    clean code, correction login

--- scripts/templates.js
+++ scripts/templates.js
@@ -272,6 +272,39 @@ function renderSubtasks() {
 }
 
 
+/**
+ * Creates HTML template for contact list item
+ * @param {Object} contact - Contact object with name, initials, colorIndex
+ * @param {string} id - Contact ID
+ * @returns {string} HTML string for contact list item
+ */
+function createContactListItemTemplate(contact, id) {
+  return `
+    <div>
+      <div class="contact-initial" style="background-image: url(./assets/icons/contact/color${contact.colorIndex}.svg)">
+        ${contact.initials}
+      </div>
+      ${contact.name}
+    </div>
+    <img src="./assets/icons/add_task/check_default.svg" alt="checkbox" />
+  `;
+}
+
+/**
+ * Creates error message template
+ * @param {string} message - Error message text
+ * @returns {string} HTML string for error message
+ */
+function createErrorMessageTemplate(message) {
+  return message;
+}
+
+// Für globalen Zugriff verfügbar machen
+if (typeof window !== 'undefined') {
+  window.createContactListItemTemplate = createContactListItemTemplate;
+  window.createErrorMessageTemplate = createErrorMessageTemplate;
+}
+
 (function injectAddTaskTemplate() {
   const render = (root = document) => {
     const container = root.querySelector('.addtask-wrapper');
